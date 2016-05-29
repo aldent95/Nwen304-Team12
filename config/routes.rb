abf8f76a649da86ee32778231d4716201469c4b0
aldent95 <aldent95@hotmail.com>
2016-05-29 15:10:45 +1200

    First webpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   resources :listings
   resources :m_users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
